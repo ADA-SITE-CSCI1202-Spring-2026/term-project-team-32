b0aadf73df9bdcfed1c09367f856134cb1d3bd4d
Qurban Qurbanov <qurbanovq0612@gmail.com>
2026-04-09 15:11:37 +0400

    Add engine and loop. (testing)

---
+++ engine/GameEngine.java
@@ -0,0 +1,11 @@
+package engine;
+
+public class GameEngine {
+    public void start(){
+        System.out.println("Started");
+    }
+    public void update(){
+        System.out.println("Updated");
+    }
+    
+}
