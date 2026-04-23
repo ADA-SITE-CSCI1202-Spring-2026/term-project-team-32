38080fff2d104369368ad13a071231085899c201
Your Name <amaharramova17296@ada.edu.az>
2026-04-23 11:16:37 +0400

    Finalizing the model package by adding the last type of task - ResearchTask

--- model/ResearchTask.java
+++ model/ResearchTask.java
@@ -1,5 +1,24 @@
 package model;
 
-public class ResearchTask {
-    
+
+import resources.Resource;
+import resources.ResourceManager;
+
+public class ResearchTask extends ColonyTask{
+     public ResearchTask(String name, int reqParts) {
+        super(name, reqParts, 500, 5, 0); 
+    }
+
+    @Override
+    public String getTaskType() { return "~Research Task~"; }
+
+    @Override
+    public boolean performTask(ResourceManager rm) {
+        if (rm.getAmount(Resource.SPARE_PARTS) >= this.reqParts) {
+            rm.setAmount(Resource.SPARE_PARTS, rm.getAmount(Resource.SPARE_PARTS) - this.reqParts);
+            rm.setAmount(Resource.CREDITS, rm.getAmount(Resource.CREDITS) + this.reward);
+            return true;
+        }
+        return false;
+    }
 }
