196b435475f92399be2230d0cb93c5a5fa44f6db
Your Name <amaharramova17296@ada.edu.az>
2026-04-29 14:15:48 +0400

    Adding processor package from branch

---
+++ processor/EngineeringBay.java
@@ -0,0 +1,14 @@
+package resources;
+
+import model.ColonyTask;
+
+public class EngineeringBay implements IProcessor {
+    @Override
+    public boolean canProcess(ColonyTask task) { return task.getTaskType().equals("~Engineering Task~"); }
+
+    @Override
+    public String processTask(ColonyTask task, ResourceManager rm) {
+        if (task.performTask(rm)) return "[Eng Bay] SUCCESS: Repaired " + task.getName();
+        return "[Eng Bay] FAILED: Not enough parts for " + task.getName();
+    }
+}
